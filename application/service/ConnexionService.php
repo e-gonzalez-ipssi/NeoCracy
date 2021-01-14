5aeaeee530162fbd351c3a98810857aca8343cbb
egonzalez <e.gonzalez@ecole-ipssi.net>
2021-01-14 14:31:43 +0100

    Ajout commentaire pour la lisibilité #21

--- application/service/ConnexionService.php
+++ application/service/ConnexionService.php
@@ -17,10 +17,22 @@ class  ConnexionService {
         $this->userManager = $userManager;
     }
 
+    /**
+     * Permet d'inscrire un utilisateur sur le site
+     *
+     */
     public function inscription() {
         
     }
 
+    /**
+     * Permet de connecté un utilisateur
+     * 
+     * @param null|string $mail Le mail de l'utilisateur qui se connecte
+     * @param null|string $password Le mot de passe de l'utilisateur qui se connect
+     * 
+     * Note : ces 2 paramêtre peuvent être nul si l'utililsateur utilise sont token pour se connecté
+     */
     public function connexion(?string $mail = null, ?string $password = null): void {
         if($this->isConnected()){
             throw new Exception("user-already-connected");
@@ -48,6 +60,11 @@ class  ConnexionService {
         }
     }
 
+    /**
+     * Permet de déconnecté l'utilisateur courant
+     * 
+     * throw Exception renvoie une exeption si il n'y a pas d'utilisateur connecté, ou si la déconnection échou
+     */
     public function deconnexion() {
         if($this->isConnected()) {
             throw new Exception("user-already-disconnected");
@@ -60,9 +77,16 @@ class  ConnexionService {
         }
     }
     
+
+    /** 
+     * Permet de vérifier si il y a un utilisateur actuellement connecté
+     * 
+     * @return bool True = connecté, False = non connecté
+     */
     public function isConnected(): bool {
         try {
-            if(! is_null($this->setCurrentUser())) {
+            $this->setCurrentUser();
+            if(! is_null($this->getCurrentUser())) {
                 return true;
             }
         } catch (Exception $e) {
@@ -72,7 +96,12 @@ class  ConnexionService {
         }
     }
 
-    public function setCurrentUser(): User {
+
+    /**
+     * Permet de set un utilisateur courant pour la fonction getCurrentUser()
+     * Utilise le cookie Utilisateur, si il n'y a pas d'utilisateur courant, le set a NULL
+     */
+    public function setCurrentUser() {
         if(isset($_COOKIE[COOKIE_USER_TOKEN])){
             try {
                 $this->me = $this->userManager->getUserByToken($_COOKIE[COOKIE_USER_TOKEN]);
@@ -82,14 +111,26 @@ class  ConnexionService {
                 }
             }
         }
-        return $this->getCurrentUser();
     }
 
-    private function setToken(User $user, string $value, int $time): void {
+
+    /**
+     * Cette fonction permet de set le token utilisateur dans la database ainsi que dans les cookie de l'utilisateur
+     * 
+     * @param User $user L'utilisateur a qui on souhaite changer le token
+     * @param string $value La valeur a laquel on souhaite set le token
+     * @param int $time temps apres laquel le token expirera (+86400 = +1 jour) 
+     */
+    private function setToken(User $user, string $value, ?int $time = null): void {
         $this->userManager->setUserToken($value, $user->getId());
         setcookie(COOKIE_USER_TOKEN, $value, $time);
     }
 
+    /**
+     * Permet de récupéré l'utilisateur courant
+     * 
+     * @return User L'utilisateur courant au format User
+     */
     private function getCurrentUser(): User {
         if (is_null($this->me)) {
             throw new Exception("error-not-connected");
