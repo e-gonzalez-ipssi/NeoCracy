b63c8ab772d04014af7b1beb5dd65d102437da4a
LouisPLN <louispoulinpro@gmail.com>
2021-02-09 12:17:19 +0100

    Button

--- install/frontend_neocracy/src/app/components/home.js
+++ install/frontend_neocracy/src/app/components/home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from "../../../src/resources/img/logo.svg"
+import logo from "../../../src/resources/img/logos.svg"
 import header from "../../../src/resources/img/header.svg"
 import footer from "../../../src/resources/img/footer.svg"
 import header2 from "../../../src/resources/img/header2.svg"
@@ -15,13 +15,13 @@ const Home = () => (
 
     <img class="z-10 absolute w-48 ml-30 mt-10" src={logo} alt="logo"></img>
 
-    <div class="w-A m-auto">
+    <div class="z-20 absolute w-full flex items-center h-screen justify-center">
 
-    <div class="flex justify-around align-center">
+    <div class="flex justify-between">
 
-    <button type="submit" class="z-20 w-B border-transparent bg-black hover:bg-yellow text-white font-bold py-2 px-4 rounded">Inscription</button>
-
-    <button type="submit" class="z-20 w-B border-transparent bg-yellow hover:bg-black text-white font-bold py-2 px-4 rounded">Connexion</button>
+    <button type="submit" class="z-20 w-90 m-6 text-black shadow bg-yellow py-4 px-4 rounded-xl">Connexion</button>
+    
+    <button type="submit" class="z-20 w-90 m-6 text-grey shadow bg-black py-4 px-4 rounded-xl">Inscription</button>
     
     </div>
     
