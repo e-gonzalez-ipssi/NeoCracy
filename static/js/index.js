270c98c52a7c9d67aded2615b52e12cbfeebe8f0
LouisPLN <louispoulinpro@gmail.com>
2021-02-10 17:36:32 +0100

    Day & Night

--- static/js/index.js
+++ static/js/index.js
@@ -1,8 +1,31 @@
 function myFunction() {
     var x =  document.getElementById("dayNight2");
+    var y =  document.getElementById("nightDay");
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
-      x.style.display = "none";
+      x.style.display = "block";
+    }
+    if (y.style.display === "none") {
+      y.style.display = "block";
+    } else {
+      y.style.display = "block";
     }
   }
+
+function myFunction2() {
+  var x =  document.getElementById("dayNight2");
+  var y =  document.getElementById("nightDay");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    x.value
+  } else {
+    x.style.display = "none";
+  }
+  if (y.style.display === "none") {
+    y.style.display = "block";
+    y.value
+  } else {
+    y.style.display = "none";
+  }
+}
