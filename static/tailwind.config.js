2d7a1c29fe4140e31f83b7a2f1c25ae4f6a890ef
LouisPLN <louispoulinpro@gmail.com>
2021-02-11 14:30:31 +0100

    Restyle

--- static/tailwind.config.js
+++ static/tailwind.config.js
@@ -43,7 +43,7 @@ module.exports = {
       'yellow-darkest': '#453411',	
       'yellow-darker': '#684f1d',	
       'yellow-dark': '#f2d024',	
-      yellow: '#ffd369',	
+      yellow: '#ffb048',	
       'yellow-light': '#fff382',	
       'yellow-lighter': '#fff9c2',	
       'yellow-lightest': '#fcfbeb',	
