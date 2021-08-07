78932d09d059ec5e5cc6057ad168ac7366e50bd1
egonzalez <e.gonzalez@ecole-ipssi.net>
2021-08-07 11:11:38 +0200

    suppréssion des characters spéciaux dans le token

--- backend/app/Service/ConnexionService.php
+++ backend/app/Service/ConnexionService.php
@@ -144,7 +144,7 @@ class  ConnexionService {
      */
     private function rand_string($length):string{ 
         $str = "";
-        $chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz@#$&*";  
+        $chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";  
         $size = strlen( $chars );  
         for( $i = 0; $i < $length; $i++ ) {  
             $str.= $chars[ rand( 0, $size - 1 ) ];  
